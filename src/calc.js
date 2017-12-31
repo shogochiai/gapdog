82a8e5f8cb6cbcb2fc3b46c1e56aa2612db14a49
Shogo Ochiai <shogo1104@gmail.com>
2017-12-31 09:21:27 +0700

    added vol

--- src/calc.js
+++ src/calc.js
@@ -22,10 +22,10 @@ rp({ uri: rateUri, transform: body=> cheerio.load(body) })
     const fns = coins.map(c=> rp({ uri: "https://vip.bitcoin.co.id/api/"+c+"_idr/ticker" }) )
     Promise.all(fns)
     .then((res) => {
-      var idrprice = res.map((str,i)=> [coins[i], parseInt(JSON.parse(str).ticker.last)/idrjpyrate] )
+      var idrprice = res.map((str,i)=> [coins[i], parseInt(JSON.parse(str).ticker.last)/idrjpyrate, Math.ceil(JSON.parse(str).ticker.vol_idr/1000/1000/idrjpyrate)+"M jpyvol"] )
 
       function cal(i){
-        return [jpyprice[i][0],Math.ceil(jpyprice[i][1]/idrprice[i][1]*1000)/1000]
+        return [jpyprice[i][0],Math.ceil(jpyprice[i][1]/idrprice[i][1]*1000)/1000, idrprice[i][2]]
       }
       var arr = [cal(0),cal(1),cal(2),cal(3),cal(4),cal(5)]
       var res = JSON.stringify(arr.sort(function(ar1,ar2){ return ar1[1] < ar2[1] ? 1 : -1 }))
